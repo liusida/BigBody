7a28ecf40e2ea5a98fa274c31f6589110aa40cf2
sida <sliu1@uvm.edu>
2020-04-08 17:40:43 -0400

    true sigmoid

--- voxelyze/evolution/cppn/CPPNActivationFunctions.py
+++ voxelyze/evolution/cppn/CPPNActivationFunctions.py
@@ -2,7 +2,9 @@ import numpy as np
 
 def sigmoid(x):
     # np.exp(large number) will cause a warning of overflow
-    return 2.0 / (1.0 + np.exp(-x)) - 1.0
+    # return 2.0 / (1.0 + np.exp(-x)) - 1.0
+    # I want to use the true sigmoid function, to avoid misleading.
+    return 1.0 / (1.0 + np.exp(-x))
 
 def neg_abs(x):
     return -np.abs(x)
